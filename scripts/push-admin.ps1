fcf4354cf151dcf6292777aa4920663e672823a3
chenjiandmw <2519333821@qq.com>
2026-01-15 15:34:43 +0800

    管理员-提交代码

--- scripts/push-admin.ps1
+++ scripts/push-admin.ps1
@@ -1,3 +1,6 @@
+git config user.name "chenjiandmw"
+git config user.email "2519333821@qq.com"
+
 git add .
 git commit -m "管理员-提交代码"
 git push --force
--- scripts/push-write.ps1
+++ scripts/push-write.ps1
@@ -1,3 +1,6 @@
+git config user.name "chenjian-29"
+git config user.email "292762749@qq.com"
+
 git add .
 git commit -m "测试用户1-提交代码"
 git push --force
